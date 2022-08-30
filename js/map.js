39b99accb7041ee87b83dae9de9a3da7f2a34954
Jérôme Le Champion <jerome.le.champion@gmail.com>
2022-08-30 20:15:47 +0200

    map

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: new google.maps.LatLng(48.860005, 2.474388),
-    zoom: 12,
+    zoom: 11,
   });
 
   const iconBase =
